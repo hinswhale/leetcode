d1d368bc5e04f636c61855fc0629c18b732134c2
hinswhale <hinswhale@yeah.net>
2023-03-23 11:39:44 +0800

    Update longest-palindromic-substring.cpp

--- longest-palindromic-substring.cpp
+++ longest-palindromic-substring.cpp
@@ -53,16 +53,18 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         string res;
-        for(int i = 0; i < s.size(); i ++)
-        {
+        int n = s.size();
+        for(int i=0; i<n; i++){
             int l = i - 1, r = i + 1;
-            while(l >= 0 && r <= s.size() && s[l] == s[r])  l --, r ++;
+            while(l >= 0 && r < s.size() && s[l] == s[r])  l--, r++;
             if(res.size() < r - l - 1) res = s.substr(l + 1, r - l - 1);
 
             l = i, r = i + 1;
-            while(l >= 0 && r <= s.size() && s[l] == s[r]) l --, r ++;
+            while(l >= 0 && r < s.size() && s[l] == s[r]) l --, r ++;
             if(res.size() < r - l - 1) res = s.substr(l + 1, r - l - 1);
+                      
         }
+
         return res;
     }
 };
